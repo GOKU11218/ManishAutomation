ece2f4908c690399deb8aa272bd3e5ca81957a4b
Administrator <Administrator@DESKTOP-PR32PP4>
2026-06-30 16:38:43 +0530

    logging changes commmited

--- Selenium/src/test/java/saucelabs/login.java
+++ Selenium/src/test/java/saucelabs/login.java
@@ -1,5 +1,6 @@
 package saucelabs;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -9,8 +10,11 @@ public class login {
      public void get() {
     	 WebDriver driver=new ChromeDriver();
     	 System.out.println("driver initialised");
-    	 driver.get("https://www.google.com");
-    	 System.out.println("google opened");
+    	 driver.get("https://www.saucedemo.com/");
+    	 driver.findElement(By.id("user-name")).sendKeys("error_user");
+    	 driver.findElement(By.id("password")).sendKeys("secret_sauce");
+    	 driver.findElement(By.id("login-button")).click();
+    	 
     	 
      }
 }
