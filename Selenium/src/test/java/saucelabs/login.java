f88d8b07016ae56d678a8e81d751823530802cc9
Administrator <Administrator@DESKTOP-PR32PP4>
2026-06-30 14:22:05 +0530

    made changes

---
+++ Selenium/src/test/java/saucelabs/login.java
@@ -0,0 +1,14 @@
+package saucelabs;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class login {
+     @Test
+     public void get() {
+    	 WebDriver driver=new ChromeDriver();
+    	 System.out.println("driver initialised");
+    	 
+     }
+}
